020e87675b3074e3b31ee52f86f1e96dfa6dff4f
Tareque Hossain <tareque@codexn.com>
2012-07-02 13:52:45 -0400

    Added project structure.

---
+++ stup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='django-remote-forms',
+    version='0.0.1',
+    description='A platform independent form serializer for Django.',
+    author='WiserTogether Tech Team',
+    author_email='tech@wisertogether.com',
+    url='http://github.com/wisertoghether/django-remote-forms/',
+    long_description=open('README.rst', 'r').read(),
+    packages=[
+        'django_remote_forms',
+    ],
+    package_data={
+    },
+    zip_safe=False,
+    requires=[
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: Pre Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
