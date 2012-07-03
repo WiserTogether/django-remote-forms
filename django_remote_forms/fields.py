3473ae3744a174ae19e31d028531ba1930711ae0
Carlo Costino <carlo.costino@wisertogether.com>
2012-07-02 23:54:21 -0400

    Added support for all of the remaining Django Form Fields and wired up the field.as_dict() functionality within the RemoteForm.

--- django_remote_forms/fields.py
+++ django_remote_forms/fields.py
@@ -1,24 +1,25 @@
 class RemoteField(object):
     """
-    A base object for being able to return a field as a dictionary.
+    A base object for being able to return a Django Form Field as a Python
+    dictionary.
 
-    It also takes into account if there is initial data for the field coming in
-    from the form directly, which overrides any initial data specified on the
-    field per Django's rules:
+    This object also takes into account if there is initial data for the field
+    coming in from the form directly, which overrides any initial data
+    specified on the field per Django's rules:
 
     https://docs.djangoproject.com/en/dev/ref/forms/api/#dynamic-initial-values
     """
 
-    def __init__(self, field, initial_data=None):
+    def __init__(self, field, form_initial_data=None):
         self.field = field
-        self.initial_data = initial_data
+        self.form_initial_data = form_initial_data
 
     def as_dict(self):
         return {
             'required': self.field.required,
             'widget': None,  # TODO:  Get widget as_dict() working.
             'label': self.field.label,
-            'initial': self.initial_data or self.field.initial,
+            'initial': self.form_initial_data or self.field.initial,
             'help_text': self.field.help_text,
             'error_messages': self.field.error_messages,
         }
@@ -48,25 +49,16 @@ class RemoteIntegerField(RemoteField):
         return field_dict
 
 
-class RemoteFloatField(RemoteField):
+class RemoteFloatField(RemoteIntegerField):
     def as_dict(self):
-        field_dict = super(RemoteFloatField, self).as_dict()
-
-        field_dict.update(
-            max_value=self.field.max_value,
-            min_value=self.field.min_value
-        )
-
-        return field_dict
+        return super(RemoteFloatField, self).as_dict()
 
 
-class RemoteDecimalField(RemoteField):
+class RemoteDecimalField(RemoteIntegerField):
     def as_dict(self):
         field_dict = super(RemoteDecimalField, self).as_dict()
 
         field_dict.update(
-            max_value=self.field.max_value,
-            min_value=self.field.min_value,
             max_digits=self.field.max_digits,
             decimal_places=self.field.decimal_places
         )
@@ -101,17 +93,47 @@ class RemoteDateTimeField(RemoteField):
         return field_dict
 
 
-class RemoteRegexField(RemoteField):
+class RemoteRegexField(RemoteCharField):
     def as_dict(self):
         field_dict = super(RemoteRegexField, self).as_dict()
 
-        field_dict.update(
-            regex=self.field.regex,
-            max_length=self.field.max_length,
-            min_length=self.field.min_length
-        )
+        field_dict.update(regex=self.field.regex)
 
         return field_dict
+
+
+class RemoteEmailField(RemoteCharField):
+    def as_dict(self):
+        return super(RemoteEmailField, self).as_dict()
+
+
+class RemoteFileField(RemoteField):
+    def as_dict(self):
+        field_dict = super(RemoteField, self).as_dict()
+
+        field_dict.update(max_length=self.field.max_length)
+
+        return field_dict
+
+
+class RemoteImageField(RemoteFileField):
+    def as_dict(self):
+        return super(RemoteImageField, self).as_dict()
+
+
+class RemoteURLField(RemoteCharField):
+    def as_dict(self):
+        return super(RemoteURLField, self).as_dict()
+
+
+class RemoteBooleanField(RemoteField):
+    def as_dict(self):
+        return super(RemoteBooleanField, self).as_dict()
+
+
+class RemoteNullBooleanField(RemoteBooleanField):
+    def as_dict(self):
+        return super(RemoteNullBooleanField, self).as_dict()
 
 
 class RemoteChoiceField(RemoteField):
@@ -121,3 +143,79 @@ class RemoteChoiceField(RemoteField):
         field_dict.update(choices=self.field.choices)
 
         return field_dict
+
+
+class RemoteTypedChoiceField(RemoteChoiceField):
+    def as_dict(self):
+        field_dict = super(RemoteTypedChoiceField, self).as_dict()
+
+        field_dict.update(coerce=self.coerce, empty_value=self.empty_value)
+
+        return field_dict
+
+
+class RemoteMultipleChoiceField(RemoteChoiceField):
+    def as_dict(self):
+        return super(RemoteMultipleChoiceField, self).as_dict()
+
+
+class RemoteTypedMultipleChoiceField(RemoteMultipleChoiceField):
+    def as_dict(self):
+        field_dict = super(RemoteTypedMultipleChoiceField, self).as_dict()
+
+        field_dict.update(coerce=self.coerce, empty_value=self.empty_value)
+
+        return field_dict
+
+
+class RemoteComboField(RemoteField):
+    def as_dict(self):
+        field_dict = super(RemoteComboField, self).as_dict()
+
+        field_dict.update(fields=self.field.fields)
+
+        return field_dict
+
+
+class RemoteMultiValueField(RemoteField):
+    def as_dict(self):
+        field_dict = super(RemoteMultiValueField, self).as_dict()
+
+        field_dict.update(fields=self.field.fields)
+
+        return field_dict
+
+
+class RemoteFilePathField(RemoteChoiceField):
+    def as_dict(self):
+        field_dict = super(RemoteFilePathField, self).as_dict()
+
+        field_dict.update(
+            path=self.field.path,
+            match=self.field.match,
+            recursive=self.field.recursive
+        )
+
+        return field_dict
+
+
+class RemoteSplitDateTimeField(RemoteMultiValueField):
+    def as_dict(self):
+        field_dict = super(RemoteSplitDateTimeField, self).as_dict()
+
+        field_dict.update(
+            input_date_formats=self.field.input_date_formats,
+            input_time_formats=self.field.input_time_formats
+        )
+
+        return field_dict
+
+
+class RemoteIPAddressField(RemoteCharField):
+    def as_dict(self):
+        return super(RemoteIPAddressField, self).as_dict()
+
+
+class RemoteSlugField(RemoteCharField):
+    def as_dict(self):
+        return super(RemoteSlugField, self).as_dict()
--- django_remote_forms/forms.py
+++ django_remote_forms/forms.py
@@ -1,3 +1,31 @@
+from django_remote_forms.fields import (
+    RemoteField,
+    RemoteCharField,
+    RemoteIntegerField,
+    RemoteFloatField,
+    RemoteDecimalField,
+    RemoteDateField,
+    RemoteDateTimeField,
+    RemoteRegexField,
+    RemoteEmailField,
+    RemoteFileField,
+    RemoteImageField,
+    RemoteURLField,
+    RemoteBooleanField,
+    RemoteNullBooleanField,
+    RemoteChoiceField,
+    RemoteTypedChoiceField,
+    RemoteMultipleChoiceField,
+    RemoteTypedMultipleChoiceField,
+    RemoteComboField,
+    RemoteMultiValueField,
+    RemoteFilePathField,
+    RemoteSplitDateTimeField,
+    RemoteIPAddressField,
+    RemoteSlugField
+)
+
+
 class RemoteForm(object):
     def __init__(self, form):
         self.form = form
@@ -22,11 +50,12 @@ class RemoteForm(object):
                     'min_length: 'number',
                     'required': False,
                     'bound_data': 'data'
+                    'widget': {
+                        'attr': 'value'
+                    }
                 }
             }
         }
-
-        This allows one to fully reconstruct the form as needed.
         """
 
         form_dict = {}
@@ -37,6 +66,25 @@ class RemoteForm(object):
         form_dict['fields'] = {}
 
         for name, field in self.form.fields.items():
-            form_dict['fields'][name] = {}  # TODO:  Call field.as_dict()
+            # Retrieve the initial data from the form itself if it exists so
+            # that we properly handle which initial data should be returned in
+            # the dictionary.
+
+            # Please refer to the Django Form API documentation for details on
+            # why this is necessary:
+            # https://docs.djangoproject.com/en/dev/ref/forms/api/#dynamic-initial-values
+            form_initial_data = self.form.initial.get(name, None)
+
+            # Instantiate the Remote Forms equivalent of the field if possible
+            # in order to retrieve the field contents as a dictionary.
+            try:
+                remote_field_class = 'Remote%s' % field.__class__.__name__
+                remote_field = remote_field_class(field, form_initial_data)
+            except Exception:
+                field_dict = {}
+            else:
+                field_dict = remote_field.as_dict()
+
+            form_dict['fields'][name] = field_dict
 
         return form_dict
